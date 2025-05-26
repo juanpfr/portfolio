8e00bda067758a940aac8390840a8e07db7a4eb8
juanpfr <juanpedrofrediani04@gmail.com>
2025-05-26 16:19:03 -0300

    first att

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Meu Script
+const menuIcon = document.querySelector(".menu-icon");
+const navList = document.querySelector("ul");
+
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("active");
+    navList.classList.toggle("active");
+});
