16e08b45ce58afad6bf936e23c609b42d48c66ab
juanpfr <juanpedrofrediani04@gmail.com>
2025-06-02 15:44:44 -0300

    project started

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,17 @@ const menuIcon = document.querySelector(".menu-icon");
 const navList = document.querySelector("ul");
 
 menuIcon.addEventListener("click", () => {
-    menuIcon.classList.toggle("active");
-    navList.classList.toggle("active");
+  menuIcon.classList.toggle("active");
+  navList.classList.toggle("active");
 });
+
+// Carrossel
+
+function scrollCarousel(direction) {
+  const container = document.getElementById("carousel");
+  const scrollAmount = 320;
+  container.scrollBy({
+    left: direction * scrollAmount,
+    behavior: "smooth"
+  });
+}
