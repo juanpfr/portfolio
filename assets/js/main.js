d4edefc2d040a72d6857137a480ab4c88907dcab
juanpfr <juanpedrofrediani04@gmail.com>
2025-06-30 20:52:42 -0300

    nova versao portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,39 @@ menuIcon.addEventListener("click", () => {
   navList.classList.toggle("active");
 });
 
-// Carrossel
-
-function scrollCarousel(direction) {
-  const container = document.getElementById("carousel");
-  const scrollAmount = 320;
-  container.scrollBy({
-    left: direction * scrollAmount,
-    behavior: "smooth"
+// Scroll
+document.querySelectorAll('nav ul li a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetID = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetID);
+    if(targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
   });
-}
+});
+
+let lastScrollTop = 0;
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scroll para baixo — esconde o nav
+    nav.classList.add('hide');
+  } else {
+    // Scroll para cima — mostra o nav
+    nav.classList.remove('hide');
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // para evitar valores negativos no topo
+});
+
+// Contador de caracteres
+const textarea = document.getElementById('message');
+const charCount = document.getElementById('charCount');
+
+textarea.addEventListener('input', () => {
+  charCount.textContent = textarea.value.length;
+});
