ef01608ad52fe32d3a87c7e575742a4babfa9ffc
juanpfr <juanpedrofrediani04@gmail.com>
2025-07-02 18:04:13 -0300

    falta estilizar os detalhes do projeto

---
+++ assets/js/projects.js
@@ -0,0 +1,83 @@
+export const projetos = [
+    // Projetos de desenvolvimento web
+    {
+        categoria: 'web',
+        nome: 'Teacher Camila',
+        imagem: 'assets/images/projects/projetoteachercamila.png',
+        descricao: 'Site com sistema de cadastro, login dos alunos agendamento de data e horário para cada alunos específico e contato para dúvidas.',
+        tecnologias: [
+            { nome: 'HTML', icone: 'assets/images/icons/html-5.png' },
+            { nome: 'CSS', icone: 'assets/images/icons/css-3.png' },
+            { nome: 'JavaScript', icone: 'assets/images/icons/js.png' },
+            { nome: 'PHP', icone: 'assets/images/icons/php.png' }
+        ],
+        github: 'https://github.com/seuusuario/portfolio',
+        deploy: null
+    },
+    {
+        categoria: 'web',
+        nome: 'TakeOne',
+        imagem: 'assets/images/projects/takeone.png',
+        descricao: 'Site com sistema de cadastro, login dos clientes agendamento de data, horário, serviços, equipamentos, espaços e equipes personalizadas com funcinário da própria TakeOne, o cliente pode montar tudo do jeito que preferir, também salvar seus próprios equipamentos no seu perfil e contato para dúvidas.',
+        tecnologias: [
+            { nome: 'HTML', icone: 'assets/images/icons/html-5.png' },
+            { nome: 'CSS', icone: 'assets/images/icons/css-3.png' },
+            { nome: 'JavaScript', icone: 'assets/images/icons/js.png' },
+            { nome: 'Python', icone: 'assets/images/icons/python.png' }
+        ],
+        github: 'https://github.com/seuusuario/portfolio',
+        deploy: null
+    },
+    {
+        categoria: 'web',
+        nome: 'Portfólio',
+        imagem: 'assets/images/projects/portfolio.png',
+        descricao: 'Meu site pessoal com HTML, CSS e JS.',
+        tecnologias: [
+            { nome: 'HTML', icone: 'assets/images/icons/html-5.png' },
+            { nome: 'CSS', icone: 'assets/images/icons/css-3.png' },
+            { nome: 'JavaScript', icone: 'assets/images/icons/js.png' }
+        ],
+        github: 'https://github.com/juanpfr/portfolio',
+        deploy: 'https://juanpfr.github.io/portfolio'
+    },
+    // projetos de desenvolvimento desktop
+    {
+        categoria: 'desktop',
+        nome: 'Teacher Camila Desktop',
+        imagem: 'assets/images/projects/projetoTeachercamilaDesktop.jpg',
+        descricao: 'Aplicativo desktop para gerenciamento dos alunos e de funcionalidades no site.',
+        tecnologias: [
+            { nome: 'CSHARP', icone: 'assets/images/icons/csharp.png' }
+        ],
+        github: 'https://github.com/juanpfr/teacherCamilaDesktop',
+        deploy: null
+    },
+    {
+        categoria: 'desktop',
+        nome: 'Auto Mestre',
+        imagem: 'assets/images/projects/projetoAutoMestre.png',
+        descricao: 'Aplicativo desktop para gerenciamento dos clientes de funcionários da mecânica.',
+        tecnologias: [
+            { nome: 'CSHARP', icone: 'assets/images/icons/csharp.png' }
+        ],
+        github: 'https://github.com/juanpfr/autoMestre',
+        deploy: null
+    },
+    // outros projetos
+    {
+        categoria: 'outros',
+        nome: 'Jogo em Pygame',
+        imagem: 'assets/images/projetos/jogo.png',
+        descricao: 'Jogo 2D com fases e chefes.',
+        tecnologias: ['Python', 'Pygame']
+    },
+    // Exercícios para estudar
+    {
+        categoria: 'exercicios',
+        nome: 'Lista de Exercícios',
+        imagem: 'assets/images/projetos/exercicios.png',
+        descricao: 'Resolução de exercícios práticos.',
+        tecnologias: ['Python']
+    },
+];
